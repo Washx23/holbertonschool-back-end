bbd718969d0a9687a5d2d924dff3f3754388426a
washx23 <5731@holbertonstudents.com>
2023-04-02 16:24:48 -0700

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+"""document """
+
 
 import json
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
     """Returns information about an employee's TODO list progress."""
 
-    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    response = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}')
     user = response.json()
 
-    response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+    response = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
     todos = response.json()
 
     # Calculate progress
@@ -18,9 +23,12 @@ def get_employee_todo_progress(employee_id):
     tasks_done = sum(1 for t in todos if t['completed'])
     titles_done = [t['title'] for t in todos if t['completed']]
 
-    print(f"Employee {user['name']} is done with tasks ({tasks_done}/{total_tasks}):")
+    print(
+        f"Employee {user['name']} \
+        is done with tasks ({tasks_done}/{total_tasks}):")
     for title in titles_done:
         print(f"\t {title}")
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
